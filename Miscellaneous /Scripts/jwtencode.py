639e0d30c83a481ab5cfc260bf1a72c866f8262b
H3ll_Ka1ser <98783688+H3llKa1ser@users.noreply.github.com>
2024-02-06 14:50:39 +0200

    Update jwtencode.py

--- Miscellaneous /Scripts/jwtencode.py
+++ Miscellaneous /Scripts/jwtencode.py
@@ -2,4 +2,4 @@ import jwt
 
 jwt.encode({"username":"admin"},'SECRET_TOKEN',algorithm='HS256')
 
-### This program encodes the values of a user account in jwt to forge a cookie to grant access
+### This program encodes the values of a user account in jwt to forge a cookie to grant access. Create a cookie named auth, then add the jwt token value in the value section.
