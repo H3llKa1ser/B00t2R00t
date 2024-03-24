bbc85d00171d3be057257b55b0e7d18b6cfb70c6
H3ll_Ka1ser <98783688+H3llKa1ser@users.noreply.github.com>
2024-03-24 16:02:29 +0200

    Create 101responseonly.py

---
+++ Miscellaneous /Scripts/101responseonly.py
@@ -0,0 +1,16 @@
+import sys
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+if len(sys.argv)-1 != 1:
+    print("""
+Usage: {} 
+    """.format(sys.argv[0]))
+    sys.exit()
+
+class Redirect(BaseHTTPRequestHandler):
+   def do_GET(self):
+       self.protocol_version = "HTTP/1.1"
+       self.send_response(101)
+       self.end_headers()
+
+HTTPServer(("", int(sys.argv[1])), Redirect).serve_forever()
