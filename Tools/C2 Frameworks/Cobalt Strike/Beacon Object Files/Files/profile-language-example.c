0a5d3e6f8eac599065930a724b5af500d6dd4802
H3ll_Ka1ser <98783688+H3llKa1ser@users.noreply.github.com>
2024-04-08 16:26:50 +0300

    Create profile-language-example.c

---
+++ Tools/C2 Frameworks/Cobalt Strike/Beacon Object Files/Files/profile-language-example.c
@@ -0,0 +1,14 @@
+[#] this is a comment
+set global_option "value";
+
+protocol-transaction {
+  set local_option "value";
+  
+  client {
+      [#] customize client indicators
+  }
+ 
+  server {
+      [#] customize server indicators
+  }
+}
