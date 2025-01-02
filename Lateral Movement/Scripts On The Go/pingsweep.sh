767fb13d529e70f28f62e687642bbf504c57d125
H3ll_Ka1ser <98783688+H3llKa1ser@users.noreply.github.com>
2025-01-02 18:45:34 +0200

    Update pingsweep.sh

--- Lateral Movement/Scripts On The Go/pingsweep.sh
+++ Lateral Movement/Scripts On The Go/pingsweep.sh
@@ -1,10 +1,1 @@
-subnet="172.17"
-(
-for i in {0..255}; do
-  for j in {1..254}; do
-    ip="$subnet.$i.$j"
-    (ping -c 1 -W 1 $ip >/dev/null 2>&1 && echo "Host $ip is up") &
-  done
-done
-wait
-)
+for i in {1..255};do (ping -c 1 172.16.2.$i | grep "bytes from"|cut -d ' ' -f4|tr -d ':' &);done
